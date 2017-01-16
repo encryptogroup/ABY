56f435da1e726bba026550a837b01835a8db4447
Daniel Demmler <daniel.demmler@gmail.com>
2017-01-16 14:06:35 +0100

    runtest_scr.sh added

---
+++ runtest_scr.sh
@@ -0,0 +1,13 @@
+#Definition of the interrupt handler routine. Invoked when Ctrl+c/z is encountered.
+exitfn () {
+    trap SIGINT              
+    kill $CLIENT_PID
+    exit                    
+}
+
+trap "exitfn" INT            # Set up SIGINT trap to call function.
+
+./bin/test-aby.exe -r 0 &
+CLIENT_PID=$!
+./bin/test-aby.exe -r 1
+	
