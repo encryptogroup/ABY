d862f2b4de9a27b115755d6caa1b342752b46d3c
Sebastian Stammler <stammler.s@gmail.com>
2018-07-07 17:17:31 +0200

    Rename to BinaryOp_v_uint32_t

--- src/abycore/circuit/booleancircuits.cpp
+++ src/abycore/circuit/booleancircuits.cpp
@@ -2134,7 +2134,7 @@ share* BooleanCircuit::PutMaxGate(share** a, uint32_t size) {
 }
 
 std::vector<uint32_t> BooleanCircuit::PutMaxGate(const std::vector<std::vector<uint32_t>>& ws) {
-	BinaryOp_uint32_t op = [this](auto a, auto b) {
+	BinaryOp_v_uint32_t op = [this](auto a, auto b) {
 				uint32_t cmp = (m_eContext == S_YAO) ?
 					PutSizeOptimizedGTGate(a, b) :
 					PutDepthOptimizedGTGate(a, b);
