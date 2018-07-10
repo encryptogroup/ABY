3839064f36581d78f8e4434c754c3c60dd67a2e7
Sebastian Stammler <stammler.s@gmail.com>
2018-07-10 19:30:27 +0200

    introduce BinaryOp_uint32_t alias

--- src/abycore/circuit/circuit.h
+++ src/abycore/circuit/circuit.h
@@ -62,6 +62,13 @@ T binary_accumulate(std::vector<T> vals,
 	return vals[0];
 }
 
+/**
+ * A binary operation on uint32_t
+ * Those are passed to binary_accumulate() as op
+ */
+using BinaryOp_uint32_t = std::function<std::vector<uint32_t>
+	(const std::vector<uint32_t>&, const std::vector<uint32_t>&)>;
+
 /** Circuit class */
 class Circuit {
 
