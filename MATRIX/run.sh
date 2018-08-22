44ce1b492fc7cf29f934c32aa3ca84260a1bdb42
liorbiu <liork.cryptobiu@gmail.com>
2018-08-22 16:03:55 +0300

    script read all the arguments

--- MATRIX/run.sh
+++ MATRIX/run.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# read the arguments without party id
+
+argc=$#
+argv=($@)
+values=""
+
+for (( j=1; j<argc; j++ )); do
+    values+=`echo ${argv[j]}`
+    values+=' '
+done
+
+# read parties file
 parties=()
 while IFS= read -r line || [[ -n "${line}" ]]; do
     l=`echo ${line} | cut -d'=' -f2`
@@ -10,4 +22,4 @@ idx=${1}
 addr=${parties[0]}
 
 cd ../build/bin
-./innerproduct_test -r ${idx} -a ${addr}
+echo "./innerproduct_test -r ${idx} -a ${addr} ${values}"
