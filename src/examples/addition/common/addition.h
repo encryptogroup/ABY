bceecef9f4431584f681980ec02f22937a6459da
llorencroma <52706876+llorencroma@users.noreply.github.com>
2019-10-31 10:18:04 +0100

    ADDITION.h

---
+++ src/examples/addition/common/addition.h
@@ -0,0 +1,56 @@
+/**
+ \file 		addition.h
+ \author 	romalvarezllorens@gmail.com
+ \copyright	ABY - A Framework for Efficient Mixed-protocol Secure Two-party Computation
+			Copyright (C) 2019 Engineering Cryptographic Protocols Group, TU Darmstadt
+			This program is free software: you can redistribute it and/or modify
+            it under the terms of the GNU Lesser General Public License as published
+            by the Free Software Foundation, either version 3 of the License, or
+            (at your option) any later version.
+            ABY is distributed in the hope that it will be useful,
+            but WITHOUT ANY WARRANTY; without even the implied warranty of
+            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+            GNU Lesser General Public License for more details.
+            You should have received a copy of the GNU Lesser General Public License
+            along with this program. If not, see <http://www.gnu.org/licenses/>.
+ \brief		Implementation of the addition  using ABY Framework.
+ */
+
+#ifndef __ADDITION_H_
+#define __ADDITION_H_
+
+#include "../../../abycore/circuit/booleancircuits.h"
+#include "../../../abycore/circuit/arithmeticcircuits.h"
+#include "../../../abycore/circuit/circuit.h"
+#include "../../../abycore/aby/abyparty.h"
+#include <math.h>
+#include <cassert>
+
+#define VALUE_A 	"A VALUE"
+#define VALUE_B 	"B VALUE"
+
+/**
+ \param		role 		role played by the program which can be server or client part.
+ \param 	address 	IP Address
+ \param 	seclvl 		Security level
+ \param 	bitlen		Bit length of the inputs
+ \param 	nthreads	Number of threads
+ \param		mt_alg		The algorithm for generation of multiplication triples
+ \param 	sharing		Sharing type object
+ \brief		This function is used for running a testing environment for solving the
+ millionaire's problem
+ */
+int32_t test_addition_circuit(e_role role, const std::string& address, uint16_t port, seclvl seclvl,
+		uint32_t bitlen, uint32_t nthreads, e_mt_gen_alg mt_alg, e_sharing sharing);
+
+/**
+ \param		s_a		shared object of alice.
+ \param		s_b 		shared object of bob.
+ \param		ac	 		arithmetic circuit object.
+ \brief		This function is used to build and solve the addition.
+ */
+share* BuildAdditionCircuit(share *s_alice, share *s_bob,
+		ArithmeticCircuit *ac);
+
+
+#endif /* __ADDITION_H_ */
