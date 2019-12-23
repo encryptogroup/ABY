7166d95029fbd8b7f106e8dd925389ad5aa641ee
llorencroma <52706876+llorencroma@users.noreply.github.com>
2019-12-23 10:08:43 +0100

    Update two_aby.cpp

--- src/examples/2ABY/common/two_aby.cpp
+++ src/examples/2ABY/common/two_aby.cpp
@@ -14,7 +14,7 @@
             You should have received a copy of the GNU Lesser General Public License
             along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
-
+#include "two_aby.h"
 #include "../../../abycore/circuit/booleancircuits.h"
 #include "../../../abycore/circuit/arithmeticcircuits.h"
 #include "../../../abycore/sharing/sharing.h"
