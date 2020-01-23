66c3600ef4d07e7c7323eda1f5e80d6d292c88d8
llorencroma <52706876+llorencroma@users.noreply.github.com>
2020-01-23 17:35:23 +0100

    Update read_csv.cpp

--- data_test/read_csv.cpp
+++ data_test/read_csv.cpp
@@ -117,7 +117,7 @@ int main(){
 //int main() {
     // Read three_cols.csv and ones.csv
     //std::vector<std::pair<std::string, std::vector<int>>> three_cols = read_csv("three_cols.csv");
-    std::vector<std::pair<std::string, std::vector<int>>> data = read_csv("data1.csv");
+   // std::vector<std::pair<std::string, std::vector<int>>> data = read_csv("data1.csv");
 
     // Write to another file to check that this was successful
     //write_csv("three_cols_copy.csv", three_cols);
