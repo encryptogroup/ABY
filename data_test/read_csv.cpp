9043c4f431a467eff4f52cd8f2a4ddf086285c91
llorencroma <52706876+llorencroma@users.noreply.github.com>
2020-01-23 17:46:17 +0100

    Update read_csv.cpp

--- data_test/read_csv.cpp
+++ data_test/read_csv.cpp
@@ -64,7 +64,7 @@ int main(){
         // Keep track of the current column index
         int colIdx = 0;
         if(colIdx == 1){
-          for (int i=0; i<v.size();i++){
+          for (int i=0; i<x_start.size();i++){
             std::cout << x_start[i] << std::endl;
           }      
         }
