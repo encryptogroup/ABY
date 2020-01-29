a2c731c886bb6bfed278558c8237fb489de83c76
llorencroma <52706876+llorencroma@users.noreply.github.com>
2020-01-29 19:17:58 +0100

    Update dataSplit.py

--- data_test/dataSplit.py
+++ data_test/dataSplit.py
@@ -13,7 +13,7 @@ import sys, getopt
 
 ## X will be splitted in x1 and x2 where x1+x2=X
 def create_shares_of_value(valueToShare):
-    random = randrange(400000)
+    random = randrange(8)
     
     result = []
     oneShare = valueToShare - random
@@ -81,7 +81,6 @@ def main(argv):
     
                 ##Creating shares of coordinate X of the point in row i
                 x_start.append(float(sheet.cell_value(i,0)))
-
                 shares = create_shares_of_value(x_start[i-1])
                 x1_s1.append(shares[0])
                 x1_s2.append(shares[1])
@@ -96,4 +95,42 @@ def main(argv):
                 
                 ##Creating shares of coordinate X_End of the point in row i
                 x_end.append(float(sheet.cell_value(i,2)))
-    
+                shares = create_shares_of_value(x_end[i-1])
+                x2_s1.append(shares[0])
+                x2_s2.append(shares[1])
+                
+                ##Creating shares of coordinate Y of the point in row i
+                y_end.append(float(sheet.cell_value(i, 3)))
+                shares = create_shares_of_value(y_end[i-1])
+                y2_s1.append(shares[0])
+                y2_s2.append(shares[1])
+        
+            wbS1 = xlsxwriter.Workbook('./dataS1.xlsx')
+            wbS2 = xlsxwriter.Workbook('./dataS2.xlsx')
+
+            sheetS1 = wbS1.add_worksheet('points')
+            sheetS2 = wbS2.add_worksheet('points')
+
+            sheetS1.write(0,0,'Share Start X')
+            sheetS1.write(0,1,'Share Start Y')
+            sheetS1.write(0,2,'Share End X')
+            sheetS1.write(0,3,'Share End Y')
+            sheetS2.write(0,0,'Share Start X')
+            sheetS2.write(0,1,'Share Start Y')
+            sheetS2.write(0,2,'Share End X')
+            sheetS2.write(0,3,'Share End Y')
+
+            sheetS1.write_column(1,0,x1_s1)
+            sheetS1.write_column(1,1,y1_s1)
+            sheetS1.write_column(1,2,x2_s1)
+            sheetS1.write_column(1,3,y2_s1)
+            sheetS2.write_column(1,0,x1_s2)
+            sheetS2.write_column(1,1,y1_s2)
+            sheetS2.write_column(1,2,x2_s2)
+            sheetS2.write_column(1,3,y2_s2)
+            wbS1.close()
+            wbS2.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
