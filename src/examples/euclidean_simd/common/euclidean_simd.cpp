de075d7f2d7bf81464cf926137f4874f6a5d7d21
llorencroma <52706876+llorencroma@users.noreply.github.com>
2020-02-03 00:31:38 +0100

    Update euclidean_simd.cpp

--- src/examples/euclidean_simd/common/euclidean_simd.cpp
+++ src/examples/euclidean_simd/common/euclidean_simd.cpp
@@ -39,7 +39,7 @@ namespace patch
 int32_t test_circuit(e_role role, const std::string& address, uint16_t port, seclvl seclvl,
 		uint32_t bitlen, uint32_t nthreads, e_mt_gen_alg mt_alg, e_sharing sharing,
 		    std::vector<long> x_start,std::vector<long> y_start,
-		    std::vector<long> x_end, std::vector<slong> y_end
+		    std::vector<long> x_end, std::vector<long> y_end
 		    ) {
 
 	/**
