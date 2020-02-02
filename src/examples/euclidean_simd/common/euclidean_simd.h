0358898d2cfff0e9d307fe83a4ddf329d09b1b3c
llorencroma <52706876+llorencroma@users.noreply.github.com>
2020-02-02 22:35:11 +0100

    Update euclidean_simd.h

--- src/examples/euclidean_simd/common/euclidean_simd.h
+++ src/examples/euclidean_simd/common/euclidean_simd.h
@@ -23,7 +23,7 @@
  millionaire's problem
  */
 int32_t test_circuit(e_role role, const std::string& address, uint16_t port, seclvl seclvl,
-		uint32_t bitlen, uint32_t nthreads, e_mt_gen_alg mt_alg, 
+		uint32_t bitlen, uint32_t nthreads, e_mt_gen_alg mt_alg, e_sharing sharing,
 		    std::vector<std::string> x_start, std::vector<std::string> y_start,
 		    std::vector<std::string> x_end, std::vector<std::string> y_end);
 /**
