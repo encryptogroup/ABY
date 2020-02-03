67d43391dfd864ca5c18309e28705de646250518
llorencroma <52706876+llorencroma@users.noreply.github.com>
2020-02-03 18:18:06 +0100

    Update euclidean_simd.cpp

--- src/examples/euclidean_simd/common/euclidean_simd.cpp
+++ src/examples/euclidean_simd/common/euclidean_simd.cpp
@@ -155,7 +155,7 @@ int32_t test_circuit(e_role role, const std::string& address, uint16_t port, sec
 
 	
 	auto start = std::chrono::high_resolution_clock::now();
-	for(int l = 0; l < 1000; l++){
+	for(int l = 0; l < 100; l++){
 		if(neighborhood.count(patch::to_string(l))==0){
 
 			//neighborhood[patch::to_string(l)]["neighbors"].push_back(0);
@@ -163,7 +163,7 @@ int32_t test_circuit(e_role role, const std::string& address, uint16_t port, sec
 			neighborhood[patch::to_string(l)]["cluster"].push_back(0);
 		}
 					
-		for (int ll = l+1; ll < 1000; ll++){
+		for (int ll = l+1; ll < 100; ll++){
 		
 			if(role == SERVER) {
 
