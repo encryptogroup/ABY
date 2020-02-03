1d4ea6f8ec997497cb4f3625763b98fe83f9d7ee
llorencroma <52706876+llorencroma@users.noreply.github.com>
2020-02-03 17:26:07 +0100

    Update euclidean_simd.cpp

--- src/examples/euclidean_simd/euclidean_simd.cpp
+++ src/examples/euclidean_simd/euclidean_simd.cpp
@@ -102,16 +102,17 @@ std::vector<std::vector<std::string> > CSVReader::getData()
 	// Iterate through each line and split the content using delimeter
 	getline(file,line);
 	std::cout<< line<< " "<<std::endl;
-	
-	while (getline(file, line))
+	int i = 0
+	while (getline(file, line) || i < 10)
 	{
 
 		std::vector<std::string> vec;
 		//line.erase(std::remove(line.begin(),line.end(),'\"'),line.end());
 		//boost::erase_all(line, "\"");
-		//std::cout<< line<< " "<<std::endl;
+		std::cout<< line<< " "<<std::endl;
 		boost::algorithm::split(vec,line, boost::is_any_of(delimeter));
 		dataList.push_back(vec);
+		++i;
 
 	}
 	// Close the File
