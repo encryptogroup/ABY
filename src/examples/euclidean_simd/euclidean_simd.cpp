5ced1328e61b2e20030252bd07f1fed5105c4700
llorencroma <52706876+llorencroma@users.noreply.github.com>
2020-02-03 01:12:34 +0100

    Update euclidean_simd.cpp

--- src/examples/euclidean_simd/euclidean_simd.cpp
+++ src/examples/euclidean_simd/euclidean_simd.cpp
@@ -104,11 +104,12 @@ std::vector<std::vector<std::string> > CSVReader::getData()
 	
 	while (getline(file, line))
 	{
-		std::cout<< line<< " "<<std::endl;
 
 		std::vector<std::string> vec;
 		//line.erase(std::remove(line.begin(),line.end(),'\"'),line.end());
 		boost::erase_all(line, "\"");
+		std::cout<< line<< " "<<std::endl;
+
 
 		boost::algorithm::split(vec,line, boost::is_any_of(delimeter));
 		dataList.push_back(vec);
