a39e4f2c3b18300eed86695367c0aa1fc487852a
llorencroma <52706876+llorencroma@users.noreply.github.com>
2020-02-04 09:34:30 +0100

    Update euclidean_simd.cpp

--- src/examples/euclidean_simd/common/euclidean_simd.cpp
+++ src/examples/euclidean_simd/common/euclidean_simd.cpp
@@ -598,12 +598,12 @@ for(int i= 0; i< keys.size();i++)
 	log << "Number of clusters: "+ patch::to_string(clusters.size())+"\n";
 	for(auto itr2 =clusters.begin (); itr2 != clusters.end (); itr2++)
 			{
-				std::cout <<"Cluster no"<< itr2->first << ": ";
+				std::cout <<"Cluster no"<< itr2->first << ": "<< first.size()<< ;
 				std::string lin;
 				// itr2->second represents vector<string> stored in map<string, vector<string>> which is stored in test.
 				for(auto itr3 = itr2->second.begin(); itr3 != itr2->second.end(); itr3++)
 				{
-					std::cout << *itr3 << ' ';
+					//std::cout << *itr3 << ' ';
 					lin = lin + "" +patch::to_string(*itr3)+ ", ";
 		
 				}
