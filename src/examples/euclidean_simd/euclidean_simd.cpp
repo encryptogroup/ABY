207e76e3f3a1a4ba2f507b54dd3ecc39cee56c9b
llorencroma <52706876+llorencroma@users.noreply.github.com>
2020-02-04 18:51:38 +0100

    Update euclidean_simd.cpp

--- src/examples/euclidean_simd/euclidean_simd.cpp
+++ src/examples/euclidean_simd/euclidean_simd.cpp
@@ -183,7 +183,7 @@ int main(int argc, char** argv) {
 
 
 	//evaluate addition cirucui using arithmetic
-	test_circuit(role, address, port, seclvl, 32,
+	test_circuit(role, address, port, seclvl, bitlen,
 			nthreads, mt_alg, S_BOOL, x_start, y_start,x_end, y_end);           
 	
 	return 0;
