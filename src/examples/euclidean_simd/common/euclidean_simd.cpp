f5ede147e2414e661399a92ddcc6dfd55e0ebecc
llorencroma <52706876+llorencroma@users.noreply.github.com>
2020-02-08 20:24:09 +0100

    Update euclidean_simd.cpp

--- src/examples/euclidean_simd/common/euclidean_simd.cpp
+++ src/examples/euclidean_simd/common/euclidean_simd.cpp
@@ -178,7 +178,7 @@ int32_t test_circuit(e_role role, const std::string& address, uint16_t port, sec
 			output = s_out->get_clear_value<uint32_t>();
 			distance = (long)output*100;
 			if(role == SERVER){
-				//std::cout<< " DISTANCE BETWEEN " <<l<<" ANS " << ll << "-->" << distance << std::endl;
+				std::cout<< " DISTANCE BETWEEN " <<l<<" ANS " << ll << "-->" << distance << std::endl;
 			}
 			
 			/** ++++++++++++ ABOVE IS OK ++++++++++++++++++++ */
