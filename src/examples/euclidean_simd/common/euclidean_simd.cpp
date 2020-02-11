c74968e62b13e114570fab37f2d5bf3e6aff5b38
llorencroma <52706876+llorencroma@users.noreply.github.com>
2020-02-11 01:02:10 +0100

    Update euclidean_simd.cpp

--- src/examples/euclidean_simd/common/euclidean_simd.cpp
+++ src/examples/euclidean_simd/common/euclidean_simd.cpp
@@ -119,17 +119,17 @@ int32_t test_circuit(e_role role, const std::string& address, uint16_t port, sec
 				
 				/**DIVIDING BY 10 I MANAGE TO REDUCE THE BILENGTH OF THE FINAL DISTANCE
 				AND BE ABLE TO USE UINT64*/ 
-				s1_x_start = circ->PutINGate((uint32_t) ceil(x_start.at(l)/10),bitlen,SERVER);
+				s1_x_start = circ->PutINGate((uint32_t) x_start.at(l),bitlen,SERVER);
 				//circ->PutPrintValueGate(s1_x_start, "X SHARE");
 				//std::cout<< " ROUNDED " << lround(x_start.at(l)/10) << std::endl;
-				s1_y_start = circ->PutINGate((uint32_t)  ceil(y_start.at(l)/10),bitlen,SERVER);
-				s1_x_end = circ->PutINGate((uint32_t)  ceil(x_end.at(l)/10),bitlen,SERVER);
-				s1_y_end = circ->PutINGate((uint32_t)  ceil(y_end.at(l)/10),bitlen,SERVER);
+				s1_y_start = circ->PutINGate((uint32_t)  (y_start.at(l),bitlen,SERVER);
+				s1_x_end = circ->PutINGate((uint32_t)  x_end.at(l),bitlen,SERVER);
+				s1_y_end = circ->PutINGate((uint32_t)  (y_end.at(l),bitlen,SERVER);
 				
-				s1_x_next_start = circ->PutINGate((uint32_t)  ceil(x_start.at(ll)/10),bitlen,SERVER);
-				s1_y_next_start = circ->PutINGate((uint32_t)   ceil(y_start.at(ll)/10),bitlen,SERVER);
-				s1_x_next_end = circ->PutINGate((uint32_t)   ceil(x_end.at(ll)/10),bitlen,SERVER);
-				s1_y_next_end = circ->PutINGate((uint32_t)  ceil(y_end.at(ll)/10),bitlen,SERVER);
+				s1_x_next_start = circ->PutINGate((uint32_t)  x_start.at(ll),bitlen,SERVER);
+				s1_y_next_start = circ->PutINGate((uint32_t)   y_start.at(ll),bitlen,SERVER);
+				s1_x_next_end = circ->PutINGate((uint32_t)   x_end.at(ll),bitlen,SERVER);
+				s1_y_next_end = circ->PutINGate((uint32_t)  y_end.at(ll),bitlen,SERVER);
 
 				s2_x_start = circ->PutDummyINGate( bitlen);
 				s2_y_start = circ->PutDummyINGate( bitlen);
@@ -142,15 +142,15 @@ int32_t test_circuit(e_role role, const std::string& address, uint16_t port, sec
 				s2_y_next_end = circ->PutDummyINGate( bitlen);
 
 			} else { 
-				s2_x_start = circ->PutINGate((uint32_t)  ceil(x_start.at(l)/10),bitlen,CLIENT);
-				s2_y_start = circ->PutINGate((uint32_t) ceil (y_start.at(l)/10),bitlen,CLIENT);
-				s2_x_end = circ->PutINGate((uint32_t)  ceil(x_end.at(l)/10),bitlen,CLIENT);
-				s2_y_end = circ->PutINGate((uint32_t) ceil(y_end.at(l)/10),bitlen,CLIENT);
+				s2_x_start = circ->PutINGate((uint32_t)  (x_start.at(l)),bitlen,CLIENT);
+				s2_y_start = circ->PutINGate((uint32_t)  (y_start.at(l)),bitlen,CLIENT);
+				s2_x_end = circ->PutINGate((uint32_t)  (x_end.at(l)),bitlen,CLIENT);
+				s2_y_end = circ->PutINGate((uint32_t) (y_end.at(l)),bitlen,CLIENT);
 				
-				s2_x_next_start = circ->PutINGate((uint32_t)  ceil(x_start.at(ll)/10),bitlen,CLIENT);
-				s2_y_next_start = circ->PutINGate((uint32_t)  ceil(y_start.at(ll)/10),bitlen,CLIENT);
-				s2_x_next_end = circ->PutINGate((uint32_t)  ceil(x_end.at(ll)/10),bitlen,CLIENT);
-				s2_y_next_end = circ->PutINGate((uint32_t) ceil(y_end.at(ll)/10),bitlen,CLIENT);
+				s2_x_next_start = circ->PutINGate((uint32_t) (x_start.at(ll)),bitlen,CLIENT);
+				s2_y_next_start = circ->PutINGate((uint32_t) (y_start.at(ll)),bitlen,CLIENT);
+				s2_x_next_end = circ->PutINGate((uint32_t)  (x_end.at(ll)),bitlen,CLIENT);
+				s2_y_next_end = circ->PutINGate((uint32_t) (y_end.at(ll)),bitlen,CLIENT);
 
 				s1_x_start = circ->PutDummyINGate( bitlen);
 				s1_y_start = circ->PutDummyINGate( bitlen);
@@ -175,8 +175,8 @@ int32_t test_circuit(e_role role, const std::string& address, uint16_t port, sec
 
 
 			//HERE WE HAVE THE 4 DISTANCE METRICS 
-			output = s_out->get_clear_value<uint32_t>();
-			distance = (long)output*100;
+			output = s_out->get_clear_value<uint64_t>();
+			distance = (long)output;
 			if(role == SERVER & (ll%300)==1 ) {
 				std::cout<< " DISTANCE BETWEEN " <<l<<" ANS " << ll << "-->" << distance << std::endl;
 			}
@@ -507,8 +507,8 @@ for(int i= 0; i< keys.size();i++)
 	share* x_next_end;
 	share* y_next_end;
 
-	uint32_t output;
-	uint32_t bitlen=32;
+	uint64_t output;
+	uint32_t bitlen=64;
 
 	share* rando;
 
