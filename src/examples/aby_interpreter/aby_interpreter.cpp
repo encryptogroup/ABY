f9db5a5a0caa3febef3d4033303afea96d483ba3
Edward Chen <edwjchen@gmail.com>
2022-02-12 22:51:06 -0500

    aby interpreter

--- src/examples/aby_interpreter/aby_interpreter.cpp
+++ src/examples/aby_interpreter/aby_interpreter.cpp
@@ -1,15 +1,15 @@
 #include <ENCRYPTO_utils/crypto/crypto.h>
 #include <ENCRYPTO_utils/parse_options.h>
-
 #include "../../abycore/aby/abyparty.h"
-
 #include "common/aby_interpreter.h"
+
+#include "argparse.hpp"
 
 enum mode {
     mpc
 };
 
-mode hash(std::string m) {
+mode hash_mode(std::string m) {
     if (m == "mpc") return mpc;
     throw std::invalid_argument("Unknown mode: "+m);
 }
@@ -26,10 +26,8 @@ std::vector<std::string> split(std::string str, char delimiter) {
 
 std::unordered_map<std::string, std::tuple<std::string, uint32_t, uint32_t>> parse_mpc_inputs(std::string test_file_path) {
     std::unordered_map<std::string, std::tuple<std::string, uint32_t, uint32_t>> input_map;
-
     std::ifstream file(test_file_path);
     assert(("Test file exists.", file.is_open()));
-
     std::string str;
     bool server_flag = false;
     bool client_flag = false;
@@ -57,12 +55,12 @@ std::unordered_map<std::string, std::tuple<std::string, uint32_t, uint32_t>> par
             uint32_t index = num_params;
             num_params++;
             if (line.size() == 2) {
-                input_map[line[0]] = std::tuple{role, value, index};
+                input_map[line[0]] = std::tuple<std::string, uint32_t, uint32_t>{role, value, index};
             } else if (line.size() > 2) {
                 // Vector input, key_idx: value
                 for (int i = 1; i < line.size(); i++) {
                     std::string key = line[0] + "_" + std::to_string(i-1);
-                    input_map[key] = std::tuple{role, value, index};
+                    input_map[key] = std::tuple<std::string, uint32_t, uint32_t>{role, value, index};
                 }
             }
         }
@@ -73,10 +71,8 @@ std::unordered_map<std::string, std::tuple<std::string, uint32_t, uint32_t>> par
 
 std::unordered_map<std::string, std::string> parse_mapping_file(std::string mapping_file_path) {
     std::unordered_map<std::string, std::string> mapping_map;
-
     std::ifstream file(mapping_file_path);
     assert(("Mapping file exists.", file.is_open()));
-
     std::string str;
     bool role_flag = false;
     while (std::getline(file, str)) {
@@ -89,30 +85,6 @@ std::unordered_map<std::string, std::string> parse_mapping_file(std::string mapp
     return mapping_map;
 }
 
-
-void check_inputs(std::string m, std::string bytecode_file_path, std::string test_file_path, std::string mapping_file_path) {
-    if (m.empty()) {
-        std::cout << "Please specify the mode: mpc" << std::endl;
-        exit(1);
-    }
-    
-    if (bytecode_file_path.empty()) {
-        std::cout << "Please specify the bytecode file path." << std::endl;
-        exit(1);
-    }
-
-    if (test_file_path.empty()) {
-        std::cout << "Please specify the test file path." << std::endl;
-        exit(1);
-    }
-
-    if (mapping_file_path.empty()) {
-        std::cout << "Please specify the mapping file path." << std::endl;
-        exit(1);
-    }
-}
-
-
 int main(int argc, char** argv) {
 	e_role role; 
 	uint32_t bitlen = 32, nvals = 31, secparam = 128, nthreads = 1;
@@ -122,36 +94,40 @@ int main(int argc, char** argv) {
 	e_mt_gen_alg mt_alg = MT_OT;
 	seclvl seclvl = get_sec_lvl(secparam);
 
-	std::vector<std::string> args(argv + 1, argv + argc);
-    std::string m, bytecode_file_path, test_file_path, mapping_file_path;
-    for (auto i = args.begin(); i != args.end(); ++i) {
-        if (*i == "-m" || *i == "--mode") {
-            m = *++i;
-        } else if (*i == "-r" || *i == "--role") {
-            role = (e_role) std::stoi(*++i);
-        } else if (*i == "-t" || *i == "--test_file") {
-            test_file_path = *++i;
-        } else if (*i == "-b" || *i == "--bytecode_file") {
-            bytecode_file_path = *++i;
-        } else if (*i == "--mapping_file") {
-            mapping_file_path = *++i;
-        }
+    argparse::ArgumentParser program("aby_interpreter");
+    program.add_argument("-M", "--mode").required().help("Mode for parsing test inputs");
+    program.add_argument("-R", "--role").required().help("Role: <Server:0 / Client:1>").scan<'i', int>();;
+    program.add_argument("-b", "--bytecode").required().help("Bytecode file");
+    program.add_argument("-t", "--test").required().help("Test inputs file");
+    program.add_argument("-m", "--mapping").required().help("Mapping of shares to circuit type file");
+
+	try {
+        program.parse_args(argc, argv);    // Example: ./main --color orange
+    }
+        catch (const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        std::exit(1);
     }
 
-    check_inputs(m, bytecode_file_path, test_file_path, mapping_file_path);
+    std::string m, bytecode_path, test_path, mapping_path;
+    m = program.get<std::string>("--mode");  
+    role = !program.get<int>("--role") ? SERVER : CLIENT;
+    bytecode_path = program.get<std::string>("--bytecode");
+    test_path = program.get<std::string>("--test");
+    mapping_path = program.get<std::string>("--mapping");
 
 	std::unordered_map<std::string, std::tuple<std::string, uint32_t, uint32_t>> params;
-    std::unordered_map<std::string, std::string> mapping;	
-
-	switch(hash(m)) {
+    std::unordered_map<std::string, std::string> mapping;
+	switch(hash_mode(m)) {
         case mpc: {
-            params = parse_mpc_inputs(test_file_path);
-            mapping = parse_mapping_file(mapping_file_path);
+            params = parse_mpc_inputs(test_path);
+            mapping = parse_mapping_file(mapping_path);
         }
         break;
     }
 
-	test_aby_test_circuit(bytecode_file_path, &params, &mapping, role, address, port, seclvl, 32,
+	test_aby_test_circuit(bytecode_path, &params, &mapping, role, address, port, seclvl, 32,
 			nthreads, mt_alg, S_BOOL);
 
 	return 0;
